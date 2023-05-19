2ae0a576fba7c390b02e12ff33ce6269f0378d9e
MujahidAkberAli <mujahidakberali@gmail.com>
2023-05-19 18:41:12 +0500

    Add project files.

---
+++ OrangeHRM_PlayWright/UnitTest1.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Playwright;
+
+
+namespace OrangeHRM_PlayWright
+{
+    public class Tests
+    {
+
+        [Test]
+        public async Task OrangeHRM_Automation()
+        {
+            var playwright = await Playwright.CreateAsync();
+
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            { Headless = false, SlowMo = 1, });
+
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+            await page.FillAsync("#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div:nth-child(2) > div > div:nth-child(2) > input", "Admin");
+            await page.FillAsync("#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div:nth-child(3) > div > div:nth-child(2) > input", "admin123");
+
+            //await page.GetByPlaceholder("Username").FillAsync("Admin");
+            //await page.GetByPlaceholder("Password").FillAsync("admin123");
+
+            await page.ClickAsync("#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button");
+            await page.ClickAsync("#app > div.oxd-layout > div.oxd-layout-navigation > aside > nav > div.oxd-sidepanel-body > ul > li:nth-child(1) > a");
+            await page.ClickAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.orangehrm-paper-container > div.orangehrm-header-container > button");
+
+            // User Role Entry
+            await page.ClickAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(1) > div > div:nth-child(2) > div > div");
+            await page.PressAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(1) > div > div:nth-child(2) > div > div", "ArrowDown");
+            await page.PressAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(1) > div > div:nth-child(2) > div > div", "ArrowDown");
+            await page.PressAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(1) > div > div:nth-child(2) > div > div", "Enter");
+
+            // Employee Name Entry
+            await page.FillAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(2) > div > div:nth-child(2) > div > div > input", "a");
+            Thread.Sleep(3000);
+            await page.PressAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(2) > div > div:nth-child(2) > div > div > input", "ArrowDown");
+            await page.PressAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(2) > div > div:nth-child(2) > div > div > input", "ArrowDown");
+            await page.PressAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(2) > div > div:nth-child(2) > div > div > input", "Enter");
+
+
+            // Status Name Entry
+            await page.ClickAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(3) > div > div:nth-child(2) > div > div");
+            await page.PressAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(3) > div > div:nth-child(2) > div > div", "ArrowDown");
+            await page.PressAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(3) > div > div:nth-child(2) > div > div", "ArrowDown");
+            await page.PressAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(3) > div > div:nth-child(2) > div > div", "Enter");
+
+            // Enter Username
+            await page.FillAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(4) > div > div:nth-child(2) > input", "Mujahid");
+
+            // Enter Password
+            await page.FillAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.oxd-form-row.user-password-row > div > div.oxd-grid-item.oxd-grid-item--gutters.user-password-cell > div > div:nth-child(2) > input", "Mujahid+5");
+
+            // Enter Confirm Password
+            await page.FillAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.oxd-form-row.user-password-row > div > div:nth-child(2) > div > div:nth-child(2) > input", "Mujahid+5");
+
+            Thread.Sleep(3000);
+            //Submit Press
+            await page.ClickAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.oxd-form-actions > button.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space");
+
+            // Search User
+            await page.FillAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(1) > div > div:nth-child(2) > input", "Mujahid");
+
+            Thread.Sleep(2000);
+            //Search Press
+            await page.ClickAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-actions > button.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space");
+
+            Thread.Sleep(3000);
+            //Pen Selection
+            await page.GetByRole(AriaRole.Button, new() { Name = "" }).ClickAsync();
+
+
+            // Editing Name
+            await page.Locator("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(4) > div > div:nth-child(2) > input").DblClickAsync();
+            await page.FillAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div:nth-child(4) > div > div:nth-child(2) > input", "Mujahid Akber Ali");
+            Thread.Sleep(2000);
+            await page.ClickAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.oxd-form-actions > button.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space");
+
+            //deleting Name
+
+            await page.FillAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(1) > div > div:nth-child(2) > input", "Mujahid Akber Ali");
+            Thread.Sleep(2000);
+            await page.ClickAsync("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-actions > button.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space");
+
+            Thread.Sleep(2000);
+
+            await page.GetByRole(AriaRole.Button, new() { Name = "" }).ClickAsync();
+            await page.ClickAsync("#app > div.oxd-overlay.oxd-overlay--flex.oxd-overlay--flex-centered > div > div > div > div.orangehrm-modal-footer > button.oxd-button.oxd-button--medium.oxd-button--label-danger.orangehrm-button-margin");
+
+
+
+            Thread.Sleep(5000);
+
+        }
+    }
+}
